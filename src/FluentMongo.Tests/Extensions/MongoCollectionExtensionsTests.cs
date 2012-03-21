1aa64cf2953700b3abedf5f3acc9fe7cc28d4009
nekresh <nekresh@gmail.com>
2012-03-21 13:28:00 +0100

    Add extension methods for Update and Delete

---
+++ src/FluentMongo.Tests/Extensions/MongoCollectionExtensionsTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using FluentMongo.Linq;
+
+namespace FluentMongo.Extensions
+{
+    [TestFixture]
+    public class MongoCollectionExtensionsTests : TestBase
+    {
+        MongoCollection<Person> Collection;
+
+        public override void SetupTest()
+        {
+            base.SetupTest();
+
+            Collection = GetCollection<Person>("people");
+
+            Collection.Insert(new Person
+            {
+                Age = 24
+            });
+            Collection.Insert(new Person
+            {
+                Age = 42
+            });
+        }
+
+        public override void TearDownTest()
+        {
+            base.TearDownTest();
+
+            Collection.RemoveAll();
+        }
+
+        [Test]
+        public void Update_Simple()
+        {
+            Collection.Update(p => p.Age < 42, Update.Inc("age", 1));
+
+            Assert.NotNull(Collection.AsQueryable().Where(p => p.Age == 25).SingleOrDefault());
+            Assert.NotNull(Collection.AsQueryable().Where(p => p.Age == 42).SingleOrDefault());
+        }
+
+        [Test]
+        public void Remove_Normal()
+        {
+            Collection.Remove(p => p.Age < 100);
+
+            Assert.AreEqual(0, Collection.Count());
+        }
+
+        [Test]
+        public void Remove_Single()
+        {
+            Collection.Remove(p => p.Age < 100, RemoveFlags.Single);
+
+            Assert.AreEqual(1, Collection.Count());
+        }
+    }
+}
---
+++ src/FluentMongo/Extensions/MongoCollectionExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using MongoDB.Driver.Builders;
+using FluentMongo.Linq;
+
+namespace MongoDB.Driver
+{
+    public static class MongoCollectionExtensions
+    {
+        public static SafeModeResult Update<TDefaultDocument>(this MongoCollection<TDefaultDocument> collection, Expression<Func<TDefaultDocument, bool>> predicate, IMongoUpdate update)
+        {
+            return collection.Update(predicate, update, UpdateFlags.None, collection.Settings.SafeMode);
+        }
+
+        public static SafeModeResult Update<TDefaultDocument>(this MongoCollection<TDefaultDocument> collection, Expression<Func<TDefaultDocument, bool>> predicate, IMongoUpdate update, SafeMode safeMode)
+        {
+            return collection.Update(predicate, update, UpdateFlags.None, safeMode);
+        }
+
+        public static SafeModeResult Update<TDefaultDocument>(this MongoCollection<TDefaultDocument> collection, Expression<Func<TDefaultDocument, bool>> predicate, IMongoUpdate update, UpdateFlags flags)
+        {
+            return collection.Update(predicate, update, flags, collection.Settings.SafeMode);
+        }
+
+        public static SafeModeResult Update<TDefaultDocument>(this MongoCollection<TDefaultDocument> collection, Expression<Func<TDefaultDocument, bool>> predicate, IMongoUpdate update, UpdateFlags flags, SafeMode safeMode)
+        {
+            var query = ((IMongoQueryable)collection.AsQueryable().Where(predicate)).GetQueryObject();
+
+            return collection.Update(new QueryComplete(query.Query), update, flags, safeMode);
+        }
+
+        public static SafeModeResult Remove<TDefaultDocument>(this MongoCollection<TDefaultDocument> collection, Expression<Func<TDefaultDocument, bool>> predicate)
+        {
+            return collection.Remove(predicate, RemoveFlags.None, collection.Settings.SafeMode);
+        }
+
+        public static SafeModeResult Remove<TDefaultDocument>(this MongoCollection<TDefaultDocument> collection, Expression<Func<TDefaultDocument, bool>> predicate, SafeMode safeMode)
+        {
+            return collection.Remove(predicate, RemoveFlags.None, safeMode);
+        }
+
+        public static SafeModeResult Remove<TDefaultDocument>(this MongoCollection<TDefaultDocument> collection, Expression<Func<TDefaultDocument, bool>> predicate, RemoveFlags flags)
+        {
+            return collection.Remove(predicate, flags, collection.Settings.SafeMode);
+        }
+
+        public static SafeModeResult Remove<TDefaultDocument>(this MongoCollection<TDefaultDocument> collection, Expression<Func<TDefaultDocument, bool>> predicate, RemoveFlags flags, SafeMode safeMode)
+        {
+            var query = ((IMongoQueryable)collection.AsQueryable().Where(predicate)).GetQueryObject();
+
+            return collection.Remove(new QueryComplete(query.Query), flags, safeMode);
+        }
+    }
+}
